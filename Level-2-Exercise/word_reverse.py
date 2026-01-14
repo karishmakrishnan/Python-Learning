2cb1413de49ee87ea5ce20e3ef3b27f87ef6654b
Karishma Krishnan <karishmakrishnan321@gmail.com>
2026-01-14 22:47:30 +0530

    Reverse the words in a string not the CHAR

--- Level-2-Exercise/word_reverse.py
+++ Level-2-Exercise/word_reverse.py
@@ -1,12 +1,10 @@
 str1 = "I love data engineering"
 str_list = str1.split()
-
 word_reverse  = []
 i = len(str_list) - 1
-
 while(i >= 0):
     word_reverse.append(str_list[i])
     i -=1
-
 word_reverse = " ".join(word_reverse)
+print("The string in reverse order: ")
 print(word_reverse)

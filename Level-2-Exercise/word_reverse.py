1ad48cc7318b81571f10d53e8a9ceadd1c4c6c25
Karishma Krishnan <karishmakrishnan321@gmail.com>
2026-01-14 22:40:45 +0530

    reverse the words in string

---
+++ Level-2-Exercise/word_reverse.py
@@ -0,0 +1,12 @@
+str1 = "I love data engineering"
+str_list = str1.split()
+
+word_reverse  = []
+i = len(str_list) - 1
+
+while(i >= 0):
+    word_reverse.append(str_list[i])
+    i -=1
+
+word_reverse = " ".join(word_reverse)
+print(word_reverse)

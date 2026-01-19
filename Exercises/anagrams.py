9ba5484aaa9643cff5bbdd1b153814f68b8b4698
Karishma Krishnan <karishmakrishnan321@gmail.com>
2026-01-19 15:09:37 +0530

    check 2 strings are anagrams

---
+++ Exercises/anagrams.py
@@ -0,0 +1,13 @@
+str1 = "karishma"
+str2 = "makarish"
+
+if len(str1) != len(str2):
+    print("Strings are not anagrams")
+elif len(str1) == len(str2):
+    # sortedstr1 = list(str1).sort()
+    sortedstr1 = sorted(str1)
+    sortedstr2 = sorted(str2)
+    if "".join(sortedstr1) == "".join(sortedstr2):
+        print("Strings are anagrams")
+    else:
+        print("Strings are not anagrams")

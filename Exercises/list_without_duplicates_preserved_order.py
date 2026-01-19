6d60b6d543605001ff4691ff7fb6a08e7338d414
Karishma Krishnan <karishmakrishnan321@gmail.com>
2026-01-19 14:31:55 +0530

    using sets and dict.fromkeys to remove duplicates

--- Exercises/list_without_duplicates_preserved_order.py
+++ Exercises/list_without_duplicates_preserved_order.py
@@ -15,4 +15,13 @@ L2_range = list(range(min_val,max_val,1))
 missing_num = [x for x in L2_range if x not in L2]
 print("missing numbers in the list:", missing_num)
 
+# Using set to remove duplicates but order not preserved
+L3 = [1,2,3,4,5,5,1,2,6,7,4,8,9]
+new_list_set = list(set(L3))    
+print("List without duplicates but order not preserved:", new_list_set) 
+# Using dict.fromkeys() to remove duplicates but order preserved
+L4 = [1,2,3,4,5,5,1,2,6,7,4,8,9]
+new_list_dict = list(dict.fromkeys(L4)) 
+print("List without duplicates and preserved order using dict:", new_list_dict)
+
 

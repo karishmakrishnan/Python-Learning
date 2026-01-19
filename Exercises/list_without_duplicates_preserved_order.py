873cee9ee9593c8866a2e3821dd9812f240b43ac
Karishma Krishnan <karishmakrishnan321@gmail.com>
2026-01-19 14:20:50 +0530

    missing values in the list

--- Exercises/list_without_duplicates_preserved_order.py
+++ Exercises/list_without_duplicates_preserved_order.py
@@ -7,4 +7,11 @@ for i in L1:
         new_list.append(i)
 print("List without duplicates and preserved order:", new_list)
 
+L2 = [1, 2, 4, 5, 6]
+min_val = min(L2)
+max_val = max(L2)
+L2_range = list(range(min_val,max_val,1))
+missing_num = [x for x in L2_range if x not in L2]
+print("missing numbers in the list:", missing_num)
+
 

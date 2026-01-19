e48abe7e6d2d760048942844e207e78a6050f497
Karishma Krishnan <karishmakrishnan321@gmail.com>
2026-01-19 15:25:14 +0530

    Longest word in the string

--- Exercises/anagrams.py
+++ Exercises/anagrams.py
@@ -12,3 +12,11 @@ elif len(str1) == len(str2):
         print("Strings are anagrams")
     else:
         print("Strings are not anagrams")
+str3 = "ADF pipelines move data efficiently"
+str_split = str3.split()
+longest_word = ""
+for word in str_split:
+    if len(word) > len(longest_word):
+        longest_word = word
+print("Longest word in the string is:",longest_word)
+print("Longest word in string is:",max(len(word) for word in str_split))

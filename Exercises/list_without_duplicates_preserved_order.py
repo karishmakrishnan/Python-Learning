89ab8e49658a74bee7ad66780780c1c3083ecdf6
Karishma Krishnan <karishmakrishnan321@gmail.com>
2026-01-19 14:48:19 +0530

    shifting the zero in a list to the end

--- Exercises/list_without_duplicates_preserved_order.py
+++ Exercises/list_without_duplicates_preserved_order.py
@@ -24,4 +24,27 @@ L4 = [1,2,3,4,5,5,1,2,6,7,4,8,9]
 new_list_dict = list(dict.fromkeys(L4)) 
 print("List without duplicates and preserved order using dict:", new_list_dict)
 
+# shifting the zero in a list to the end
+L = [0, 1, 0, 3, 12]
+for x in L:
+    if x == 0:
+        L.remove(x)
+        L.append(0)
+print("After shifting 0 to the end:",L)
+# Alternative approach
+int_L = []
+zero_L = []
+for x in L:
+    if x == 0:
+        zero_L.append(x)
+    else:
+        int_L.append(x)
+L_zero_end = int_L+zero_L
+print("After shifting 0 to the end:",L_zero_end)
 
+# Using list comprehension
+L5 = [0, 1, 0, 3, 12]   
+L_no_zero = [x for x in L5 if x != 0]
+zero_list = [x for x in L5 if x == 0]   
+L5_zero_end = L_no_zero + zero_list
+print("After shifting 0 to the end using list comprehension:",L5_zero_end)

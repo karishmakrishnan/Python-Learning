0d54c5d037efedf4d5c1de138ce7868f6d7e1cca
Karishma Krishnan <karishmakrishnan321@gmail.com>
2026-01-21 21:48:33 +0530

    palindrom using loop and reverse list using loop and index

--- Algorithms/palindron_using_loop.py
+++ Algorithms/palindron_using_loop.py
@@ -1,11 +1,27 @@
 #  palindron using loop
-str1 = "malayalam"
+str1 = "papp"
 
 len_str = len(str1)
 right = 0
 left = -1
+is_palindrom = True
 
-while 
+while right < len_str //2:
+    if str1[right] != str1[left]:
+        is_palindrom = False
+        break
+    right +=1
+    left -=1
+print(f"The string {str1} is palindrom: {is_palindrom}")
 
+# Reverse List
+list1 = [1,2,3,4,5,6,6,7,8,9]
+rev_list = []
+i = len(list1) - 1
 
-
+while(i >= 0):
+    rev_list.append(list1[i])
+    i -=1
+    
+print(f"Actual list:",list1)
+print("Reverse of list:", rev_list)

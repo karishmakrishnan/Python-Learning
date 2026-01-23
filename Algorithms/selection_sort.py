54ae379c40aa3a48c91f91d17d4ccaf7dfa4055d
Karishma Krishnan <karishmakrishnan321@gmail.com>
2026-01-23 22:27:55 +0530

    minor changes

--- Algorithms/selection_sort.py
+++ Algorithms/selection_sort.py
@@ -1,3 +1,22 @@
 # selection sort
 arr = [7,2,4,2,5,6,5,8,3,9]
 key = arr[0]
+j = float('-inf')
+for i in range(1,len(arr)-1,1):
+    j = i+1
+    # key = arr[i - 1]
+    if j <= len(arr) - 1:
+        if key > arr[i]:
+            temp = key
+            key = arr[i]
+            arr[i] = temp
+        else:
+            key = arr[i]
+        if key > arr[j]:
+            temp = key
+            key = arr[j]
+            arr[j] = key
+        else:
+            key = arr[j]
+print(f"Array after the sort is:", arr)
+        

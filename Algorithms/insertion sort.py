f1d9b3f9ea32f285963b259b24392177b2c33a35
Karishma Krishnan <karishmakrishnan321@gmail.com>
2026-01-24 18:29:04 +0530

    insertion sort completed

--- Algorithms/insertion sort.py
+++ Algorithms/insertion sort.py
@@ -5,21 +5,11 @@ j = float('-inf')
 for i in range(1,len(arr),1):
     j = i -1
     key = arr[i]
-    k = i+1
-    # print(i)
-    print(key)
-    while j >=0 & arr[j] > key:
+    # print(key)
+    while j >=0 and arr[j] > key:
         # 1. shift
         arr[j+1] = arr[j]
-        # insert key 
-        arr[j] = key
         j -=1
-        # print(arr)
-    # if (k <= len(arr)-1):  
-    #     if (arr[k] > key):
-    #     # shift
-    #         arr[k - 1] = arr[k]
-    #         arr[k] = key
-    #         # print(arr)
+    arr[j+1] = key
 print(f"Array after the sort is:", arr)
         

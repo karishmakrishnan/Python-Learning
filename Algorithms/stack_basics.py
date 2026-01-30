a8bf557b2a4b985322fda9b7467c97aa011bd551
Karishma Krishnan <karishmakrishnan321@gmail.com>
2026-01-30 22:34:58 +0530

    Improved version

--- Algorithms/stack_basics.py
+++ Algorithms/stack_basics.py
@@ -12,3 +12,23 @@ if len(stack) == 0:
     print("valid")
 else:
     print("Invalid")
+
+# improved version
+s = "(())("
+stack = []
+is_valid = True
+
+for char in s:
+    if char == '(':
+        stack.append(char)
+    elif char == ')':
+        if stack:
+            stack.pop()
+        else:
+            is_valid = False
+            break
+
+if is_valid and not stack:
+    print("Valid")
+else:
+    print("Invalid")

73c042a3e02c0ebc40b85c429cd7d3501e438ef8
Karishma Krishnan <karishmakrishnan321@gmailcom>
2026-01-31 19:33:04 +0530

    Updated version

--- Algorithms/stack_problems.py
+++ Algorithms/stack_problems.py
@@ -42,3 +42,16 @@ for i in range(len(arr)):
             break
         j += 1
 print(next_max)
+
+# updated version 
+arr = [4, 5, 2, 10]
+result = [-1] * len(arr)
+stack = []
+
+for i in range(len(arr)):
+    while stack and arr[i] > arr[stack[-1]]:
+        idx = stack.pop()
+        result[idx] = arr[i]
+    stack.append(i)
+
+print(result)

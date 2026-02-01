d73625a8750082ec01bc5656fa4fa0cbcb6ec4fb
Karishma Krishnan <karishmakrishnan321@gmailcom>
2026-02-01 10:43:24 +0530

    tree creation, inorder, preorder, postorder with examples

--- Algorithms/trees_in_python.py
+++ Algorithms/trees_in_python.py
@@ -18,7 +18,40 @@ class Node:
         self.right = None
 # DFS (Depth first search)/ Inorder(left->root->right)
 # 4 2 5 1 3
-# preorder/ root-?left->right
+# preorder/ root->left->right
 # 12453
 # postorder left->right->root
 # 45231
+
+# inorder
+def inorder(root):
+    if not root:
+        return
+    inorder(root.left)
+    print(root.val, end= " ")
+    inorder(root.right)
+def preorder(root):
+    if not root:
+        return
+    print(root.val, end= " ")
+    preorder(root.left)
+    preorder(root.right)
+def postorder(root):
+    if not root:
+        return
+    postorder(root.left)
+    preorder(root.right)
+    print(root.val, end= " ")
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+
+inorder(root)
+print("\n")
+preorder(root)
+print("\n")
+postorder(root)

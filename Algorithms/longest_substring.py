92b824e8041c99dd631a3b18520c073ae326f936
Karishma Krishnan <karishmakrishnan321@gmail.com>
2026-02-06 15:35:27 +0530

    longest substring

---
+++ Algorithms/longest_substring.py
@@ -0,0 +1,13 @@
+# longest substring
+s = "abcabcbb"
+
+left = 0
+seen = set()
+max_len = 0
+
+for right in range(len(s)):
+    # shrink window if duplicate
+    # add current char
+    # update max_len
+    
+    pass

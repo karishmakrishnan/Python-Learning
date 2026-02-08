e7b7b818a082fe19963478831cd48698649a18eb
Karishma Krishnan <karishmakrishnan321@gmail.com>
2026-02-08 12:30:47 +0530

    find the substring in given string

---
+++ Algorithms/substr_in_str.py
@@ -0,0 +1,17 @@
+# find the substring in given string 
+
+str1 = "dogcar"
+target = "car"
+
+stack = []
+rev_target = list(reversed(target))
+
+print(rev_target)
+
+for s in str1:
+    stack.append(s)
+    if(stack and stack[-1] != rev_target[-1]):
+        stack.pop()
+    elif(rev_target and stack[-1] == rev_target[-1]):
+        rev_target.pop()
+print(stack)

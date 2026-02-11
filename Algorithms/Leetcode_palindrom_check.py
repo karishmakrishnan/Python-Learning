ec54bf8c0ef9c8f1fb2a4e202118615db74e101c
Karishma Krishnan <karishmakrishnan321@gmail.com>
2026-02-11 11:30:23 +0530

    leetcode clean the string and check if palindrom or not

---
+++ Algorithms/Leetcode_palindrom_check.py
@@ -0,0 +1,59 @@
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+# Example 2:
+
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+# Example 3:
+
+# Input: s = " "
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric characters.
+# Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+
+import string
+import re
+
+import re
+import string
+class Solution(object):
+    def clean_string(self, s):
+        lower_str = s.lower()
+        chars = r"[a-z]+"
+        list_chars = re.findall(chars, lower_str)
+        cleaned_str = "".join(list_chars)
+        return cleaned_str
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        is_palindrom = True
+        
+        left = 0 
+        if s == " " or s == "":
+            is_palindrom = True
+        else:
+            cleaned = self.clean_string(s)
+            right = len(cleaned) - 1
+            while(left != right):
+                if cleaned[left] != cleaned[right]:
+                    is_palindrom = False
+                    break
+                else:
+                    left += 1
+                    right -= 1
+          
+        return is_palindrom

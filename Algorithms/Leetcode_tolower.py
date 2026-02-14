cea2ad7f45345db4a12c21c6fbb1f5b4a666ab7f
Karishma Krishnan <karishmakrishnan321@gmail.com>
2026-02-14 18:53:33 +0530

    Given a string s, return the string after replacing every uppercase letter with the same lowercase letter

---
+++ Algorithms/Leetcode_tolower.py
@@ -0,0 +1,38 @@
+
+# Code
+# Code
+# Code Sample
+# Testcase
+# Testcase
+# Test Result
+# 709. To Lower Case
+# Solved
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+ 
+
+# Example 1:
+
+# Input: s = "Hello"
+# Output: "hello"
+# Example 2:
+
+# Input: s = "here"
+# Output: "here"
+# Example 3:
+
+# Input: s = "LOVELY"
+# Output: "lovely"
+
+
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        s = s.lower()
+        # print(s)
+        return s
